07f03e20080976e60e57f7004c9c7e538cf6a544
wchengk09 <wchengk09@163.com>
2026-05-05 11:11:35 +0800

    chore: 将 prisma migrations 从 .gitignore 中移除

---
+++ prisma/migrations/20260505023409_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Countdown" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "targetDate" TIMESTAMP(3) NOT NULL,
+    "backgroundColor" TEXT,
+    "backgroundImage" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Countdown_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PersonalCountdown" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "targetDate" TIMESTAMP(3) NOT NULL,
+    "backgroundColor" TEXT,
+    "backgroundImage" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PersonalCountdown_pkey" PRIMARY KEY ("id")
+);
